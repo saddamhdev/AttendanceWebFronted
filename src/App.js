21a22f141af203b68364c008cf467c0df2bc8eda
Mehrabcd <mehrabcd23gmail.com>
2025-02-20 19:17:42 +0600

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Home from "./components/Home";
-import Error from "./components/Error";
-import RegistrationForm from "./components/RegistrationForm";
+import AppRoutes from "./routes/AppRoutes";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/error" element={<Error />} />
-        <Route path="/reg" element={<RegistrationForm />} />
-      </Routes>
-    </Router>
-  );
-}
+const App = () => {
+  return <AppRoutes />;
+};
 
 export default App;
